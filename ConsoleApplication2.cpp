76ceab0a6c22313e183e0e3fe74053ff6bc00cdb
FelixVoro <115408780+FelixVoro@users.noreply.github.com>
2022-10-11 18:45:25 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int w;
+	cin >> w;
+	if (w % 2 == 0)
+		if (w != 2)
+			cout << "YES";
+		else
+			cout << "NO";
+	else
+		cout << "NO";
+}
+/*#include<iostream>
+using namespace std;
+int main() {
+	int w;
+	int a;
+	cin >> w;
+	a = w / 2;
+	if (w % 2 == 1)
+		cout << "NO";
+	else
+		if (a % 2 == 1)
+			cout << "NO";
+		else
+			cout << "YES";
+
+}*/
+
